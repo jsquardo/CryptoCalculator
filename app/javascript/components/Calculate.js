b2f2223dc54a3149a4495ba2031ae10468113b65
Johnny Squardo <squ@rdo.host>
2020-03-02 05:57:17 +0000

    Setup base React

---
+++ app/javascript/components/Calculate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculate extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <div>Calculate goes here</div>;
+	}
+}
+
+export default Calculate;
